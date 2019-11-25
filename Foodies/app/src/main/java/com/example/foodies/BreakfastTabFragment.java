8260c4aa8200f751fb1ca692a4bbe1f83fc2fc41
Jyoti Avhad <avhadjyoti09@gmail.com>
2019-11-25 16:29:01 +0530

    Implementing Search Tab View UI

--- Foodies/app/src/main/java/com/example/foodies/BreakfastTabFragment.java
+++ Foodies/app/src/main/java/com/example/foodies/BreakfastTabFragment.java
@@ -4,12 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BreakfastTabFragment extends Fragment {
+
+    GridView gridViewList;
+
+    ArrayList<Integer> breakfastItemList = new ArrayList<>(Arrays.<Integer>asList(
+
+            R.drawable.upma,
+            R.drawable.bread_pakoda,
+            R.drawable.misal_pav,
+            R.drawable.besan_chilla,
+            R.drawable.methi_thepla,
+            R.drawable.gobi_paratha,
+            R.drawable.dosa,
+            R.drawable.idli,
+            R.drawable.aloo_paratha
+
+            ));
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        return inflater.inflate(R.layout.fragment_tab_breakfast, container, false);
+
+
+
     }
 }
