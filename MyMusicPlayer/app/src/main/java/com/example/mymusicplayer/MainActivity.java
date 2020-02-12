be429beb77092f4980cd8482bcd07c38d1e5053a
Jyoti Avhad <avhadjyoti09@gmail.com>
2020-02-12 18:49:29 +0530

    Implement Music Player

---
+++ MyMusicPlayer/app/src/main/java/com/example/mymusicplayer/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mymusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playButton, pauseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+
+//        playButton = findViewById(R.id.btn_start);
+//        pauseButton = findViewById(R.id.btn_stop);
+
+        ImageView iv_play_pause = findViewById(R.id.btn_stop);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "The Song is Over", Toast.LENGTH_SHORT).show();
+                    }
+                });
+//                playButton.setVisibility(View.GONE);
+
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
