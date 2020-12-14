6d318eea75522669137c617b60149fc8b2727191
JyotiAvhad <avhadjyoti09@gmail.com>
2020-12-14 12:00:00 +0530

    home screen added

---
+++ NeoStore/app/src/main/java/activity/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.neostore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button register_user=findViewById(R.id.btn_register);
+
+        register_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ NeoStore/app/src/main/java/com/example/neostore/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.neostore;
+
+public class ViewPagerAdapter {
+}
